4521b8f0e30de4e422ffc6acc19e19d0c61c3920
Jhon Tabio <JhonTabioCS@gmail.com>
2024-02-29 00:42:28 -0500

    Read user input after pressing entered and caught data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import './App.css'
+import React, { useState } from 'react';
 import CommandInput from './components/CommandInput'
+import './App.css'
 
 function App()
 {
+  const [cmdValue, setCmdValue] = useState('');
+  const [cmdData, setCmdData] = useState('');
+
+  const handleOnChange = (newValue: string) => {
+    setCmdValue(newValue);
+  };
+
+  const handleKeyDown = () => {
+    setCmdData(`You entered: ${cmdValue}`);
+  };
+
   return (
     <>
       <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet"/>
 
       <div id="terminal">
-        <CommandInput/>
+        {cmdData && <div>{cmdData}</div>}
+        <CommandInput value={cmdValue} onChange={handleOnChange} onKeyDown={handleKeyDown}/>
       </div>
     </>
   )
--- src/components/CommandInput.tsx
+++ src/components/CommandInput.tsx
@@ -1,7 +1,21 @@
-function CommandInput()
+interface CommandInput
 {
+  value: string;
+  onChange: (value: string) => void;
+  onKeyDown: () => void;
+}
+
+const CommandInput: React.FC<CommandInput> = ({value, onChange, onKeyDown}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === "Enter") onKeyDown();
+  };
+
   return(
-      <div id="cl">[client@portfolio ~]$ <input className="commandInput" name="cmd" autoFocus/></div>
+      <div id="cl">[client@portfolio ~]$ <input className="commandInput" name="cmd" type="text" value={value} onChange={handleChange} onKeyDown={handleKeyDown} autoFocus/></div>
   );
 }
 
