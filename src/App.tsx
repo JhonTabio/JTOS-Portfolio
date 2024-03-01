74b86264b7458d8f5bb32492ef9024177ae47c6c
Jhon Tabio <JhonTabioCS@gmail.com>
2024-03-01 17:57:06 -0500

    Added a somewhat functioning command history. Will work on later

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { processCommand } from "./utils/utils";
 import CommandInput from "./components/CommandInput"
 import "./App.css"
@@ -6,16 +6,39 @@ import "./App.css"
 function App()
 {
   const [cmdValue, setCmdValue] = useState("");
-  const [cmdHistory, setList] = useState<string[]>([]);
+  const [cmdHistory, setList] = useState<string[]>([""]);
+  const [historyIndex, setHistoryIndex] = useState(0);
 
   const handleOnChange = (newValue: string) => {
     setCmdValue(newValue);
+    cmdHistory[0] = newValue;
+    setHistoryIndex(0);
   };
 
   const handleKeyDown = () => {
     setList(currentList => [...currentList, cmdValue]);
     setCmdValue("");
+    setHistoryIndex(0);
+    cmdHistory[0] = "";
   };
+  
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowUp')
+        setHistoryIndex((prevIndex) => (prevIndex === 0 ? cmdHistory.length - 1 : (prevIndex - 1 === 0 ? 1 : prevIndex - 1)));
+      else if (event.key === 'ArrowDown')
+        setHistoryIndex((prevIndex) => (prevIndex === cmdHistory.length - 1 ? 0 : (prevIndex === 0 ? 0 : prevIndex + 1)));
+
+      setCmdValue(cmdHistory[historyIndex]);
+      cmdHistory[0] = cmdHistory[historyIndex];
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [cmdHistory.length]);
 
   return (
     <>
@@ -23,7 +46,7 @@ function App()
 
       <div id="terminal">
         <ul id="history">
-          {cmdHistory.map((item, index) => (
+          {cmdHistory.slice(1).map((item, index) => (
             <li key={index}>
               [<span className="client">
                   client
@@ -39,11 +62,11 @@ function App()
               </span>]$
               <span className="command">&nbsp;{item}</span>
               <br/>
-              {processCommand(item)}
+              {processCommand(item)}<br/>{cmdHistory[historyIndex]}
               </li>
           ))}
         </ul>
-        <CommandInput value={cmdValue} onChange={handleOnChange} onKeyDown={handleKeyDown}/>
+        <CommandInput value={cmdHistory[historyIndex]} onChange={handleOnChange} onKeyDown={handleKeyDown}/>
       </div>
     </>
   )
