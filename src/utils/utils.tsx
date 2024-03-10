fdd90156a9e4ba34555db9033fa055e28bc3cc74
Jhon Tabio <JhonTabioCS@gmail.com>
2024-03-10 03:21:15 -0400

    Added banner command

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -161,6 +161,19 @@ export const processCommand = (cmd: string): JSX.Element => {
       else
         ret = changeColor(process.splice(1));
       break;
+    case "BANNER":
+      ret =
+        <span className="command"> 
+              _____  _________    ___     ______   
+            |_   _||  _   _  | .'   `. .' ____ \  
+              | |  |_/ | | \_|/  .-.  \| (___ \_| 
+          _   | |      | |    | |   | | _.____`.  
+          | |__' |     _| |_   \  `-'  /| \____) | 
+          `.____.'    |_____|   `.___.'  \______.' 
+                                         
+          <br/>
+        </span>
+      break;
     default:
       ret = <span><em style={{color: "red"}}>bash: {process[0]}: command not found. Try help</em></span>
       break;
