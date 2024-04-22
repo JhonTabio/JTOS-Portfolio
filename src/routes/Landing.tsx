45eeab851f8bff981048a2b8b80c0449785a1b34
Jhon Tabio <JhonTabioCS@gmail.com>
2024-04-22 17:49:37 -0400

    Added keyboard input and page transition

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -1,12 +1,45 @@
 import { useState, useEffect } from "react"
+import { useLocation } from "wouter";
 import "./Landing.css"
 
 function Landing() {
 
+  const [location, setLocation] = useLocation();
   const [animate, setAnimation] = useState("intro");
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const options = [
+    { name: "Command Line Interface experience", link: "/CLI" },
+    { name: "Graphical User Interface experience", link: "/GUI" },
+    { name: "Help", action: () => console.log("Information!") }
+  ];
 
   const [arrow, setArrow] = useState<string>('-');
+
+  // Keyboard input
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if(e.key === "ArrowDown")
+        setSelectedIndex((selectedIndex + 1) % options.length);
+      else if(e.key === "ArrowUp")
+        setSelectedIndex((selectedIndex - 1 + options.length) % options.length);
+
+      if (e.key === "Enter")
+      {
+        const option = options[selectedIndex];
+
+        if (option.link) setLocation(option.link);
+        else if (option.action) option.action();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [selectedIndex, options, setLocation]);
     
+    // Arrow animation
     useEffect(() => {
         const interval = setInterval(() => {
             setArrow(currentChar => currentChar === '>' ? '—' : '>');
@@ -19,7 +52,7 @@ function Landing() {
     <>
       <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet"/>
 
-      <div id="container" class={`${animate}`}>
+      <div id="container" className={`${animate}`}>
         <pre id="banner" style={{"color": "purple"}}>
           ███╗     ██╗████████╗   ███████╗██╗  ██╗██████╗ ███████╗██████╗ ██╗███████╗███╗   ██╗ ██████╗███████╗███╗<br/>
           ██╔╝     ██║╚══██╔══╝   ██╔════╝╚██╗██╔╝██╔══██╗██╔════╝██╔══██╗██║██╔════╝████╗  ██║██╔════╝██╔════╝╚██║<br/>
@@ -38,13 +71,20 @@ function Landing() {
           </strong>
         </p>
 
-        <p id="info" style={{width: "50%"}}>
+        <div id="info" style={{width: "50%"}}>
           <span style={{color: "lightblue"}}>?</span> Admin Dashboard | Welcome Page<br/>
-          <ul>
-            Command Line Interface experience<br/>
-            Graphical User Interface experience
+          <ul style={{listStyleType: "None", margin: 0, padding: "0em 1em"}}>
+            {options.map((e, i) => (
+              <li key={i} onClick={() => {
+                if(e.link) setLocation(e.link);
+                else if(e.action) e.action();
+              }} onMouseEnter={() => setSelectedIndex(i)}
+                style={{ cursor: "pointer", color: selectedIndex === i ? "lightslategray" : "inherit"}}>
+                {(selectedIndex === i ? "> " : '') + e.name}
+              </li>
+            ))}
           </ul>
-        </p>
+        </div>
 
       </div>
     </>
