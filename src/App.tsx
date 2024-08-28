e6cbc0c83905f6891d1e1925adb99d2c36d4e852
Jhon Tabio <JhonTabioCS@gmail.com>
2024-08-27 20:44:29 -0400

    Started GUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'wouter';
 import './App.css'
 import Landing from './routes/Landing';
 import CLI from './routes/CLI';
+import GUI from "./routes/GUI"
 import { DirProvider } from './components/CommandDir';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" component={Landing}/>
         <DirProvider>
           <Route path="/cli" component={CLI}/>
+          <Route path="/gui" component={GUI}/>
         </DirProvider>
       </Switch>
     </>
---
+++ src/routes/GUI.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "wouter";
+import "./GUI.css"
+
+function GUI()
+{
+  return (
+    <>
+      <div id="folder">
+        <div id="sidebar">
+          <h1> TESTTINGNGG</h1>
+          <p>another</p>
+        </div>
+      </div>
+    </>
+  )
+}
+export default GUI
