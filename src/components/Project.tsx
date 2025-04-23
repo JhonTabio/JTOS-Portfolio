b6cf59283c0acdd43a1b2c7dba88b008e567ded4
Jhon Tabio <JhonTabioCS@gmail.com>
2025-04-23 18:05:04 -0400

    Added svgs for watchers, stars and issues in project details

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,14 +7,27 @@ interface ProjectProps
 
 export function Project({ project }: ProjectProps)
 {
+  const eye = <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true">
+    <path fill="white" d="M8 2c1.981 0 3.671.992 4.933 2.078 1.27 1.091 2.187 2.345 2.637 3.023a1.62 1.62 0 0 1 0 1.798c-.45.678-1.367 1.932-2.637 3.023C11.67 13.008 9.981 14 8 14c-1.981 0-3.671-.992-4.933-2.078C1.797 10.83.88 9.576.43 8.898a1.62 1.62 0 0 1 0-1.798c.45-.677 1.367-1.931 2.637-3.022C4.33 2.992 6.019 2 8 2ZM1.679 7.932a.12.12 0 0 0 0 .136c.411.622 1.241 1.75 2.366 2.717C5.176 11.758 6.527 12.5 8 12.5c1.473 0 2.825-.742 3.955-1.715 1.124-.967 1.954-2.096 2.366-2.717a.12.12 0 0 0 0-.136c-.412-.621-1.242-1.75-2.366-2.717C10.824 4.242 9.473 3.5 8 3.5c-1.473 0-2.825.742-3.955 1.715-1.124.967-1.954 2.096-2.366 2.717ZM8 10a2 2 0 1 1-.001-3.999A2 2 0 0 1 8 10Z"></path>
+  </svg>
+
+  const star = <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true">
+    <path fill="white" d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
+</svg>
+
+  const issue = <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true">
+    <path fill="white" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path><path fill="white" d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
+</svg>
   const projectData = openProject(project);
 
   return (
     <div className="gui_project_container">
       <div className="gui_project_desc">
         <h4>Quick description - <a href={projectData?.html_url} target="_blank" rel="noopener noreferrer">{projectData?.html_url}</a></h4>
+        <p>{new Date(projectData!.created_at).toLocaleString("en-US", { year: "numeric", month: "long" })}</p>
         <p>{projectData?.description ?? "No description"}</p>
         {projectData?.homepage !== "https://N/A" && <p>Homepage: <a href={projectData?.homepage!} target="_blank" rel="noopener noreferrer">{projectData?.homepage}</a></p>}
+        {eye} {projectData!.watchers_count} {star} {projectData!.stargazers_count} {issue} {projectData!.open_issues_count}
       </div>
     </div>
   );
