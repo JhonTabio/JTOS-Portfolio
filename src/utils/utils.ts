94cb8f2016ece5f522bed5f9ab1ee894630f76db
JhonTabio <JhonTabioCS@gmail.com>
2025-05-13 16:38:43 -0400

    Moved icons into utils file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -329,6 +329,28 @@ export const sliderChar: string = `
 ╚══════╝
 `
 
+import React, { ReactElement } from "react";
+
+export const folderIcon: ReactElement = React.createElement(
+  "svg",
+  { className: "gui_icon", viewBox: "0 0 512 512", fill: "#6e54a6" },
+  React.createElement("path", {
+    d: "M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z",
+  })
+);
+
+export const fileIcon: ReactElement = React.createElement(
+  "svg",
+  {
+    className: "gui_icon",
+    viewBox: "0 0 384 512",
+    fill: "#604996",
+  },
+  React.createElement("path", {
+    d: "M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z",
+  })
+);
+
 const isColor = (color: string) =>
 {
   const element = document.createElement("div");
