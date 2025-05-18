156aece86ca4cab0121a6641f4a4a9f10211757e
JhonTabio <JhonTabioCS@gmail.com>
2025-05-18 17:52:11 -0400

    Moved things around for windowbar, folder icon always visible and opens new window

--- src/components/WindowBar.tsx
+++ src/components/WindowBar.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useWindowManager } from "./WindowContext";
+import { folderIcon } from "../utils/utils";
+import { FileExplorer } from "./FileExplorer";
 
 export function WindowBar()
 {
-  const { windows, toggleWindow } = useWindowManager();
+  const { windows, createWindow, toggleWindow } = useWindowManager();
   const [hoveredApp, setHoveredApp] = useState<string | null>(null);
 
   const grouped = windows.reduce((acc, win) => {
@@ -12,33 +14,63 @@ export function WindowBar()
     return acc;
   }, {} as Record<string, typeof windows>);
 
- return (
-    <div className="gui_windowbar">
-      {Object.entries(grouped).map(([app, appWindows]) => (
-        <div
-          key={app}
-          className="gui_windowbar_icon"
-          onMouseEnter={() => setHoveredApp(app)}
-          onMouseLeave={() => setHoveredApp(null)}
-          onClick={() => { if (appWindows.length === 1) toggleWindow(appWindows[0].id) }}
-        >
-          {hoveredApp === app && (
-            <div className="gui_windowbar_dropdown">
-              {appWindows.map(win => (
-                <div
-                  key={win.id}
-                  className={`gui_dropdown_item ${win.minimized ? "" : "opened"} ${win.focus ? "focused" : ""}`}
-                  onClick={() => {toggleWindow(win.id)}}
-                >
-                  {win.title}
-                </div>
-              ))}
-            </div>
-          )}
-          {appWindows[0].icon}
+  const fileManagerWindows = grouped["FileExplorer"] || [];
+  const otherApps = Object.keys(grouped).filter(app => app !== "FileExplorer");
 
+  return (
+    <>
+      {hoveredApp && grouped[hoveredApp] && (
+        <div className="gui_windowbar_panel">
+          {grouped[hoveredApp].map((win) => (
+            <div
+              key={win.id}
+              className={`gui_panel_item ${win.minimized ? "" : "opened"} ${win.focus ? "focused" : ""}`}
+              onClick={() => toggleWindow(win.id)}
+            >
+              {win.title}
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+
+      <div className="gui_windowbar">
+        <div
+          key="FileExplorer"
+          className="gui_windowbar_icon"
+          onMouseEnter={() => setHoveredApp("FileExplorer")}
+          onMouseLeave={() => setHoveredApp(null)}
+          onClick={() => {
+            if (fileManagerWindows.length === 0)
+              createWindow("Home", (id: number) => <FileExplorer id={id}/>, "FileExplorer", folderIcon);
+            else if (fileManagerWindows.length === 1)
+              toggleWindow(fileManagerWindows[0].id);
+          }}
+        >
+          {folderIcon}
+        </div>
+
+        {otherApps.map((app) => {
+          const appWindows = grouped[app] || [];
+
+          if (appWindows.length === 0) return null;
+
+          const icon = appWindows[0]?.icon;
+
+          return (
+            <div
+              key={app}
+              className="gui_windowbar_icon"
+              onMouseEnter={() => setHoveredApp(app)}
+              onMouseLeave={() => setHoveredApp(null)}
+              onClick={() => {
+                if (appWindows.length === 1) toggleWindow(appWindows[0].id);
+              }}
+            >
+              {icon}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
