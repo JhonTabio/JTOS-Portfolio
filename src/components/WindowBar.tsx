0c2add1d771da060e6a76d20d1a4494a984eaac2
JhonTabio <JhonTabioCS@gmail.com>
2025-10-01 21:41:52 -0400

    Added an exit button back to landing page from GUI page

--- src/components/WindowBar.tsx
+++ src/components/WindowBar.tsx
@@ -1,11 +1,14 @@
 import { useWindowManager } from "./WindowContext";
+import { useLocation } from "wouter";
 
 export function WindowBar()
 {
+  const [_, setLocation] = useLocation();
   const { windows, toggleWindow, focusWindow } = useWindowManager();
 
   return (
     <div className="gui_windowbar">
+      <button onClick={() => setLocation("/")}>Shut down</button>
       {windows.map((tab) => (
         <div className={`gui_windowbar_element ${tab.focus ? "focused" : ""}`}
           onClick={() => {tab.focus ? toggleWindow(tab.id) : focusWindow(tab.id)}}>{tab.title}</div>
